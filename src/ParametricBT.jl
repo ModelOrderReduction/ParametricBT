76b0d11262010d78b0fa4d3c5ef7b2d5469ecd99
Algopaul <72927083+Algopaul@users.noreply.github.com>
2022-08-04 17:13:07 +0200

    implement until first iteration

--- src/ParametricBT.jl
+++ src/ParametricBT.jl
@@ -1,5 +1,47 @@
 module ParametricBT
 
-# Write your package code here.
+using FiniteDifferences, LinearAlgebra, MatrixEquations
+
+function compute_taylor_coeffs(A, x0, order)
+    @assert length(x0) == 1 "Only implemented for scalar x0"
+    @assert order >= 0 "Order must be non-negative"
+    Acoeffs = Vector{typeof(A(x0))}(undef, 0)
+    for i = 0:order
+        Ai = zeros(size(A(x0))...)
+        for j in eachindex(Ai)
+            Ai[j] = central_fdm(5, i)(s -> A(s)[j], x0[1])
+        end
+        Ai ./= factorial(i)
+        push!(Acoeffs, Ai)
+    end
+    return Acoeffs
+end
+
+function balancing(A, B, C, order, x0)
+    Ac = compute_taylor_coeffs(A, x0, order)
+    Bc = compute_taylor_coeffs(B, x0, order)
+    Cc = compute_taylor_coeffs(C, x0, order)
+    P0, R = symlyapc(Ac[1], Bc[1])
+    Q0, L = symlyapc(Ac[1]', Cc[1]')
+    U, Sig, V = svd(R'*L)
+    Tl = L * V * diagm(0 => 1 ./ sqrt.(Sig))
+    Tr = R * U * diagm(0 => 1 ./ sqrt.(Sig))
+    Tr = Tr'
+    for l in 1:(order+1)
+    end
+    return nothing
+end
+
+function symlyapc(A, B)
+    P = plyapc(A, B)
+    Pc = P*P'
+    return Pc, P
+end
+function symlyapc(A::Adjoint, B::Adjoint)
+    P = plyapc(A, B)
+    Pc = P'*P
+    return Pc, P'
+end
+
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,35 @@
-using ParametricBT
+include("../src/ParametricBT.jl")
+using .ParametricBT
+using Random
 using Test
 
-@testset "ParametricBT.jl" begin
-    # Write your tests here.
+function generate_test_system(n, m, p)
+    A0 = rand(MersenneTwister(0), n, n);
+    A0 = -A0'*A0
+    A1 = rand(MersenneTwister(1), n, n);
+    A1 = -A1'*A1
+    A2 = rand(MersenneTwister(2), n, n);
+    A2 = -A2'*A2
+    A(x) = A0+A1*x+A2*x^2
+    B(x) = rand(MersenneTwister(3), n, m)
+    C(x) = rand(MersenneTwister(3), p, n)
+    return A, B, C
 end
+
+
+@testset "ParametricBT.jl" begin
+    f(x) = x^3
+    A0 = rand(MersenneTwister(0), 5, 5);
+    A1 = rand(MersenneTwister(1), 5, 5);
+    A2 = rand(MersenneTwister(2), 5, 5);
+    A(x) = A0+A1*x+A2*x^2
+    x0 = 0.0
+    Acoeffs = ParametricBT.compute_taylor_coeffs(A, x0, 2)
+    @test length(Acoeffs) == 3
+    @test Acoeffs[1] ≈ A0
+    @test Acoeffs[2] ≈ A1
+    @test Acoeffs[3] ≈ A2
+end
+
+    A, B, C = generate_test_system(5, 2, 3)
+    ParametricBT.balancing(A, B, C, 2, 0.0)
